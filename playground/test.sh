fb4a3a922d5870b4a246c14dd904e2c55bd82b6d
Alex Bezroutchko <abb@gremwell.com>
2014-11-08 23:16:51 +0100

    test.sh: make output less verbose

--- playground/test.sh
+++ playground/test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/expect
 
-eval spawn ssh -p 2201 brandon@localhost curl -I http://www.example.com
+eval spawn ssh -p 2201 brandon@localhost curl -s -I http://www.example.com
 #use correct prompt
 set prompt ":|#|\\\$"
 interact -o -nobuffer -re $prompt return
