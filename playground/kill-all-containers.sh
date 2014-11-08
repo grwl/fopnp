863413841611798a061220f7fd30e9b4f82bd9d1
Alex Bezroutchko <abb@gremwell.com>
2014-11-08 04:39:40 +0100

    %

--- playground/kill-all-containers.sh
+++ playground/kill-all-containers.sh
@@ -1,12 +1,20 @@
 #!/bin/sh
 
-[ "$1" = "YES" ] || exit
+if [ "$1" != "YES" ] ; then
+	echo "Usage: $0 YES"
+	exit 2
+fi
 
 # stop all containers
 docker stop -t=0 `sudo docker ps -q`
 
 # remove all container
-docker rm `sudo docker ps -aq`
-
+#docker rm `sudo docker ps -aq`
 #docker rmi `sudo docker images 
+
+# remove interfaces created by launch.sh
+ifaces="h1-eth1 h2-eth1 h3-eth1 h4-eth1 exampleCOM homeA homeB modemA-eth1 modemB-eth1 example-eth1 ftp-eth0 mail-eth0 www-eth0"
+for i in $ifaces; do
+	ip link delete $i
+done
 
