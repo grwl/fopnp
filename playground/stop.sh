fe8d89f6418cb43674322ffd1d98cc8a9546361d
Alex Bezroutchko <abb@gremwell.com>
2014-11-08 05:23:12 +0100

    %

--- playground/stop.sh
+++ playground/stop.sh
@@ -8,9 +8,9 @@ docker ps -a | grep fopnp/ | awk '{ print $1}' | xargs --no-run-if-empty docker 
 
 #docker rmi `sudo docker images 
 
-echo Removing remove interfaces created by launch.sh ...
-ifaces="h1-eth1 h2-eth1 h3-eth1 h4-eth1 exampleCOM homeA homeB modemA-eth1 modemB-eth1 example-eth1 ftp-eth0 mail-eth0 www-eth0"
-for i in $ifaces; do
-	ip link delete $i
-done
+#echo Removing remove interfaces created by launch.sh ...
+#ifaces="h1-eth1 h2-eth1 h3-eth1 h4-eth1 exampleCOM homeA homeB modemA-eth1 modemB-eth1 example-eth1 ftp-eth0 mail-eth0 www-eth0"
+#for i in $ifaces; do
+#	ip link delete $i
+#done
 
